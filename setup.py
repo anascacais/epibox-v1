6551a75533f2302661f3362fb6a760f09743ceca
Ana Sofia Carmo <37815826+anascacais@users.noreply.github.com>
2021-02-04 11:31:22 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="epibox", 
-    version="0.0.2",
+    version="0.0.1",
     author="Ana Sofia Carmo",
     author_email="anascacais@gmail.com",
     description="EpiBOX: A novel approch to long-term monitoring in Epilepsy",
